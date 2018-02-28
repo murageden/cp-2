5310a4c2c13e6c3d36c7b1711efdd22f282f12fb
dennis-murage <muragemd@gmail.com>
2018-02-28 14:40:43 +0300

    Add functionality for the user registration route

--- api/v1/we_connect/routes.py
+++ api/v1/we_connect/routes.py
@@ -3,12 +3,15 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # local imports
-from we_connect.business import Business
-from we_connect.review import Review
 from we_connect.user import User
 
 # creates a user account
 @app.route('/weconnect/api/v1/auth/register', methods=['POST'])
 def create_user():
-    pass
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user = User()
+    message = user.create_user(name, email, password)
+    return jsonify(message)
     
