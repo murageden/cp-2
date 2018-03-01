9619633943dbfb2d451eff1b1bc248bb217c5cff
dennis-murage <muragemd@gmail.com>
2018-03-01 23:08:48 +0300

    Add tests for viewing, updating business

--- api/v1/tests/test.py
+++ api/v1/tests/test.py
@@ -23,7 +23,38 @@ class BusinessTestCase(unittest.TestCase):
     
     def test_create_business(self):
         self.response = self.business.create_business(
-            'Test Business', 'Supermarket', 'Behind Equity Bank', 'Githurai, Nairobi Area', 4)
+            'Test Business1', 'Supermarket', 'Behind Equity Bank', 'Githurai, Nairobi Area', 4)
         self.assertEqual(self.response['msg'],
         'Business id {} created successfully'.format(
             self.response['id']))
+    def test_returns_a_business_if_business_exists(self):
+        self.response_add = self.business.create_business(
+            'Test Business2', 'Supermarket', 'Behind Equity Bank', 'Githurai, Nairobi Area', 4)
+        self.response_read = self.business.view_business(
+            self.response_add['id'])
+        self.assertEqual(self.response_read['msg'], 'okay')
+
+    # This applies for updating, deleting, viewing
+    # a non-existing business
+    def test_returns_a_message_if_business_does_not_exist(self):
+        self.response = self.business.view_business(1000)
+        self.assertEqual(
+            self.response['msg'], 'Business id {} not found'.format(
+                self.response['id']))
+
+    def test_updates_a_business_for_owner_if_business_exists(self):
+        self.response_add = self.business.create_business(
+            'Test Business3', 'Supermarket', 'Behind Equity Bank', 'Githurai, Nairobi Area', 4)
+        self.response_modify = self.business.update_business(
+            self.response_add['id'], 'Modify Business', 'Shop', 'Behind Equity Bank', 'Githurai, Nairobi Area', 4)
+        self.assertEqual(
+            self.response_modify['msg'], 'Business id {} modified successfully'.format(
+                self.response_modify['id']))
+
+    def test_returns_error_if_updater_is_not_owner(self):
+        self.response_add = self.business.create_business(
+            'Test Business4', 'Supermarket', 'Behind Equity Bank', 'Githurai, Nairobi Area', 4)
+        self.response_modify = self.business.update_business(
+            self.response_add['id'], 'Modify Business', 'Shop', 'Behind Equity Bank', 'Githurai, Nairobi Area', 5)
+        self.assertEqual(
+            self.response_modify['msg'], 'You cannot update this business')
