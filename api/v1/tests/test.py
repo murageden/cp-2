bfb3ce870d6335a1440bb517d2df293580a804b3
dennis-murage <muragemd@gmail.com>
2018-03-01 09:31:26 +0300

    Fixed failing test

--- api/v1/tests/test.py
+++ api/v1/tests/test.py
@@ -9,8 +9,9 @@ class UserTestCase(unittest.TestCase):
     def test_create_user(self):
         self.response = self.user.create_user(
             'Test User', 'test@user.com', '1234pass')
-        self.assertEqual(self.response['msg'], 'User id {}\
-        created successfully'.format(self.response['id']))
+        self.assertEqual(self.response['msg'],
+        'User id {} created successfully'.format(
+            self.response['id']))
 
     def test_get_all_users(self):
         pass
