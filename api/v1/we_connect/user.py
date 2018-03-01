84b160fb4d6b748d97375c6bfc60ff4b09955ffd
dennis-murage <muragemd@gmail.com>
2018-03-01 14:14:11 +0300

    Modify user registration function

--- api/v1/we_connect/user.py
+++ api/v1/we_connect/user.py
@@ -6,16 +6,18 @@ class User(object):
 
     def create_user(self, name, email, password):
         self.id = 1
-        if len(self.users):
-            self.id = self.users[-1].id + 1
-        self.name = name
-        self.email = email
-        self.password = password
-        self.users.append(self)
+        if len(User.users):
+            self.id = User.users[-1].id + 1
+        self.user_dict = {
+            'id': self.id,
+            'name': name;
+            'email': email,
+            'password': password}
+        self.users.append(self.user_dict)
         return {
             'id': self.id,
             'msg': 'User id {} created successfully'.format(self.id)
             }
 
     def get_all_users(self):
-        return self.users
+        return User.users
