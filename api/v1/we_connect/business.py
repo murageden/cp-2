028e48803c13f2bd3862fa58b5ff544f7b25c8ae
dennis-murage <muragemd@gmail.com>
2018-03-01 23:07:21 +0300

    Add functions for reading and updating a business

--- api/v1/we_connect/business.py
+++ api/v1/we_connect/business.py
@@ -15,7 +15,8 @@ class Business(object):
             'category': category,
             'description': description,
             'location': location,
-            'ownerId': ownerId
+            'ownerId': ownerId,
+            'msg': 'okay'
         }
         Business.businesses.append(self.business_dict)
         return {
@@ -23,3 +24,33 @@ class Business(object):
             'msg': 'Business id {} created successfully'.format(
                 self.id)
         }
+
+    # reads a business
+    def view_business(self, id):
+        for business in Business.businesses:
+            if business['id'] == id:
+                return business
+        return {
+            'id': id,
+            'msg': 'Business id {} not found'.format(id)
+        }
+
+    # updates a business
+    def update_business(
+        self, id, name, category, description, location, ownerId):
+        response = self.view_business(id)
+        if not response['ownerId'] == ownerId:
+            return {
+                'status': 'error',
+                'msg': 'You cannot update this business'
+            }
+        if response['msg'] == 'okay':
+            response['name'] = name
+            response['category'] = category
+            response['description'] = description
+            response['location'] = location
+            return {
+                'id': id,
+                'msg': 'Business id {} modified successfully'.format(id)
+            }
+        return response
