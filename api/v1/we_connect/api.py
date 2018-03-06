c9d57feccd7777be905f4cf6e478b1529262ff6e
dennis-murage <muragemd@gmail.com>
2018-03-06 23:53:27 +0300

    Add entry point for the app

---
+++ api/v1/we_connect/api.py
@@ -0,0 +1,12 @@
+# entry point for the API
+from flask import Flask
+
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'p9Bv<3Eid9%$i01'
+
+
+if __name__ == '__main__':
+    app.run()
