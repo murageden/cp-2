b767f8bcb938d559cd64db595f7e1b53bae417d8
dennis-murage <muragemd@gmail.com>
2018-03-07 02:17:09 +0300

    Fix code for review module

--- api/v1/we_connect/review.py
+++ api/v1/we_connect/review.py
@@ -13,17 +13,21 @@ class Review(object):
         self.id = 1
         self.user = User()
         self.created_on = str(datetime.now())
+
         if not len(Review.reviews) == 0:
             self.id = Review.reviews[-1]['id'] + 1
+
         self.review_dict = {
             'id': self.id,
             'rating': rating,
             'body': body,
-            'review by': self.user.view_user(userId),
+            'review by': User.view_user(userId),
             'businessId': businessId,
             'created on': self.created_on
         }
+
         self.reviews.append(self.review_dict)
+
         return {
             'review': self.review_dict,
             'msg': "Review created ok"
@@ -32,12 +36,10 @@ class Review(object):
     # view a business' review
     def view_reviews_for(self, businessId):
         reviews_for = []
+
         for review in Review.reviews:
             if review['businessId'] == businessId:
                 reviews_for.append(review)
+
         if reviews_for:
             return reviews_for
-        return {
-            'status': 'error',
-            'msg': f'No reviews found for business id {businessId}'
-        }
