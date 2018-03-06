6bf16264f1e7353ed2846f7eda21d8fdaef796b9
dennis-murage <muragemd@gmail.com>
2018-03-06 12:17:26 +0300

    Add methods for the review module

--- api/v1/we_connect/review.py
+++ api/v1/we_connect/review.py
@@ -1,24 +1,32 @@
+from datetime import datetime
+from we_connect.user import User
+
+
 class Review(object):
     reviews = []
+
     def __init__(self):
         pass
 
     # adds a review
     def add_review(self, rating, body, userId, businessId):
         self.id = 1
+        self.user = User()
+        self.created_on = str(datetime.now())
         if not len(Review.reviews) == 0:
             self.id = Review.reviews[-1]['id'] + 1
         self.review_dict = {
             'id': self.id,
             'rating': rating,
             'body': body,
-            'userId': userId,
-            'businessId': businessId
+            'review by': self.user.view_user(userId),
+            'businessId': businessId,
+            'created on': self.created_on
         }
-        Review.reviews.append(self.review_dict)
+        self.reviews.append(self.review_dict)
         return {
-            'id': self.id,
-            'msg': f'Review id {self.id} created successfully'
+            'review': self.review_dict,
+            'msg': "Review created ok"
         }
 
     # view a business' review
