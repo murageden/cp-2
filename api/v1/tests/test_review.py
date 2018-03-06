456ba04ce772e4135c90ea7bcdb30ac2185080e8
dennis-murage <muragemd@gmail.com>
2018-03-07 02:16:27 +0300

    Add tests for review module

--- api/v1/tests/test_review.py
+++ api/v1/tests/test_review.py
@@ -7,48 +7,18 @@ from we_connect.business import Business
 class reviewTestCase(unittest.TestCase):
     def setUp(self):
         self.review = Review()
-        self.user = User()
-        self.business = Business()
 
     def test_create_review(self):
-        # create a user
-        self.response_user = self.user.add_user(
-            'Test User', 'test@user.com', '1234pass')
-        self.userId = self.response_user['user']['id']
-
-        # create a business for this user
-        self.response_bs = self.business.add_business(
-            'Test Business1', 'Supermarket',
-            'Behind Equity Bank', 'Githurai, Nairobi Area', self.userId)
-        self.businessId = self.response_bs['business']['id']
-
-        # add review by this user and for this business
         self.response = self.review.add_review(
-            4, "Nice place for holidays", self.userId, self.businessId)
+            4, "Nice place for holidays", 4, 1)
 
-        # check whether this review exists in the data structure
         self.assertIn(self.response['review'], Review.reviews)
 
     def test_returns_all_reviews_for_business(self):
-        # create a user
-        self.response_user = self.user.add_user(
-            'Test User', 'test@user.com', '1234pass')
-        self.userId = self.response_user['user']['id']
-
-        # create a business for this user
-        self.response_bs = self.business.add_business(
-            'Test Business1', 'Supermarket',
-            'Behind Equity Bank', 'Githurai, Nairobi Area', self.userId)
-        self.businessId = self.response_bs['business']['id']
-
-        # add two reviews by this user and for this business
         self.review.add_review(
-            4, "Nice place for holidays", self.userId, self.businessId)
+            4, "Nice place for holidays", 4, 1)
 
         self.review.add_review(
-            1, "Chefs are rude", self.userId, self.businessId)
+            1, "Chefs are rude", 4, 2)
 
-        # check whether there are two reviews for this business
-        # in the data structure
-        self.reviews_for_bs = self.review.view_reviews_for(self.businessId)
-        self.assertTrue(len(self.reviews_for_bs) == 2)
+        self.assertTrue(len(self.review.view_reviews_for(1)) == 2)
