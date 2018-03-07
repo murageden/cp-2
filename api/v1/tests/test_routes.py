b1b5e64f4efc9c995a257217a3be325b69338899
dennis-murage <muragemd@gmail.com>
2018-03-07 12:04:50 +0300

    Add test case for the log in endpoint

--- api/v1/tests/test_routes.py
+++ api/v1/tests/test_routes.py
@@ -1,4 +1,6 @@
 import unittest
+import jwt
+
 from flask import jsonify, json
 from we_connect.routes import app
 from we_connect.user import User
@@ -16,6 +18,11 @@ class EndpointsTestCase(unittest.TestCase):
                 "password": "1234usr"
             }
 
+        self.test_login = {
+                "username": "test1",
+                "password": "1234usr"
+        }
+
     def test_create_user_endpoint(self):
         self.response = self.client.post('/weconnect/api/v1/auth/register',
 
@@ -25,3 +32,18 @@ class EndpointsTestCase(unittest.TestCase):
 
         self.assertEqual(self.response.status_code, 201)
         self.assertIn("Test Name", str(self.response.data))
+
+    def test_login_user_endpoint(self):
+        self.response = self.client.post('/weconnect/api/v1/auth/login',
+
+                    data=json.dumps(self.test_login),
+
+                    headers={'content-type': 'application/json'})
+
+        self.response = json.loads(self.response.data)
+
+        data = jwt.decode(self.response['token'], app.config['SECRET_KEY'])
+
+        user = User.view_user(data['username'])
+
+        self.assertEqual(user['email'], self.test_user['email'])
