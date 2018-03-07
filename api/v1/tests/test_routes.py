169a031b1b5faf0b8ec678ce5efcddb830436b6e
dennis-murage <muragemd@gmail.com>
2018-03-07 09:27:56 +0300

    Modify test cases

--- api/v1/tests/test_routes.py
+++ api/v1/tests/test_routes.py
@@ -9,12 +9,19 @@ class EndpointsTestCase(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client()
 
+        self.test_user = {
+                "name": "Test Name",
+                "email": "test@test1.com",
+                "username": "test1",
+                "password": "1234usr"
+            }
+
     def test_create_user_endpoint(self):
         self.response = self.client.post('/weconnect/api/v1/auth/register',
 
                     data=json.dumps(self.test_user),
 
-                    headers={'content-type': 'application/json'}
+                    headers={'content-type': 'application/json'})
 
         self.assertEqual(self.response.status_code, 201)
-        self.assertIn("Test User", str(self.response.data))
+        self.assertIn("Test Name", str(self.response.data))
