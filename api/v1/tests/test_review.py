15aeb15a5bb34110eb79d3900b5bdb6dfda14907
dennis-murage <muragemd@gmail.com>
2018-03-08 00:13:55 +0300

    Remove unncessary white spaces

--- api/v1/tests/test_review.py
+++ api/v1/tests/test_review.py
@@ -11,14 +11,11 @@ class reviewTestCase(unittest.TestCase):
     def test_create_review(self):
         self.response = self.review.add_review(
             4, "Nice place for holidays", 4, 1)
-
         self.assertIn(self.response, Review.reviews)
 
     def test_returns_all_reviews_for_business(self):
         self.review.add_review(
             4, "Nice place for holidays", 4, 1)
-
         self.review.add_review(
             1, "Chefs are rude", 4, 2)
-
         self.assertTrue(len(self.review.view_reviews_for(1)) == 2)
