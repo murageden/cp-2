cc92e23d0ccd6ab02be6ebdd1f4844b8f1fa1af7
dennis-murage <muragemd@gmail.com>
2018-03-08 00:14:55 +0300

    Remove unnecessary blank lines

--- api/v1/tests/test_business.py
+++ api/v1/tests/test_business.py
@@ -24,41 +24,32 @@ class BusinessTestCase(unittest.TestCase):
         self.response = self.business.add_business(
             'Test Business3', 'Supermarket',
             'Behind Equity Bank', 'Githurai, Nairobi Area', 4)
-
         self.business.update_business(
             self.response['id'],
             'Modify Business', 'Shop',
             'Behind Equity Bank', 'Eldoret')
-
         self.new_business = self.business.view_business(
             self.response['id'])
-
         self.assertFalse(self.new_business['name'] == 'Test Business3')
-
         self.assertIn(self.new_business, Business.businesses)
 
     def test_deletes_a_business_if_exists(self):
         self.response = self.business.add_business(
             'Test Business4', 'Shop',
             'Best Prices', 'Mombasa', 4)
-
         self.business.delete_business(
             self.response['id'])
-
         self.assertFalse(self.business.view_business(
             self.response['id']))
-
         self.assertNotIn(self.business.view_business(
             self.response['id']), Business.businesses)
 
     def test_search_business_by_location(self):
         self.response = self.business.search_business_by_location(
             'Githurai, Nairobi Area')
-
         self.assertTrue(len(self.response) == 2)
 
     def test_search_business_by_category(self):
         self.response = self.business.search_business_by_category(
             'Supermarket')
-
         self.assertTrue(len(self.response) == 2)
