f0c03ec48c341050cb391ea0c1c9b559ae777e9c
dennis-murage <muragemd@gmail.com>
2018-03-08 23:37:28 +0300

    Fix failing tests in business model

--- api/v1/tests/test_business.py
+++ api/v1/tests/test_business.py
@@ -5,17 +5,21 @@ from we_connect.business import Business
 class BusinessTestCase(unittest.TestCase):
     def setUp(self):
         self.business = Business()
+        self.test_user = self.user.add_user(
+            'Test User', 'test@user.com', 'test_user', '1234pass')
 
     def test_add_business(self):
         self.response = self.business.add_business(
             'Test Business1', 'Supermarket',
-            'Behind Equity Bank', 'Githurai, Nairobi Area', 4)
+            'Behind Equity Bank', 'Githurai,
+            'Nairobi Area', self.test_user['username'])
         self.assertIn(self.response, Business.businesses)
 
     def test_returns_a_business_if_business_exists(self):
         self.response_add = self.business.add_business(
             'Test Business2', 'Supermarket',
-            'Behind Equity Bank', 'Githurai, Nairobi Area', 4)
+            'Behind Equity Bank',
+            'Githurai, Nairobi Area', self.test_user['username'])
         self.response_read = self.business.view_business(
             self.response_add['id'])
         self.assertIn(self.response_read, Business.businesses)
@@ -23,7 +27,8 @@ class BusinessTestCase(unittest.TestCase):
     def test_updates_a_business__if_exists(self):
         self.response = self.business.add_business(
             'Test Business3', 'Supermarket',
-            'Behind Equity Bank', 'Githurai, Nairobi Area', 4)
+            'Behind Equity Bank', 'Githurai,
+            Nairobi Area', self.test_user['username'])
         self.business.update_business(
             self.response['id'],
             'Modify Business', 'Shop',
@@ -36,7 +41,7 @@ class BusinessTestCase(unittest.TestCase):
     def test_deletes_a_business_if_exists(self):
         self.response = self.business.add_business(
             'Test Business4', 'Shop',
-            'Best Prices', 'Mombasa', 4)
+            'Best Prices', 'Mombasa', self.test_user['username'])
         self.business.delete_business(
             self.response['id'])
         self.assertFalse(self.business.view_business(
