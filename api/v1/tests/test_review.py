198f71f38ad9066d1faae884f0d21655c27428ef
dennis-murage <muragemd@gmail.com>
2018-03-09 00:00:21 +0300

    Fix failing tests in review model

--- api/v1/tests/test_review.py
+++ api/v1/tests/test_review.py
@@ -7,15 +7,27 @@ from we_connect.business import Business
 class reviewTestCase(unittest.TestCase):
     def setUp(self):
         self.review = Review()
+        self.user = User()
+        self.test_user = self.user.add_user(
+            'Test User', 'test@user.com', 'test_user', '1234pass')
+        self.business = Business()
+        self.test_bs = self.business.add_business(
+            'Test Business1', 'Supermarket',
+            'Behind Equity Bank',
+            'Githurai, Nairobi Area', self.test_user['username'])
 
     def test_create_review(self):
         self.response = self.review.add_review(
-            4, "Nice place for holidays", 4, 1)
+            4, "Nice place for holidays",
+            self.test_user['username'], self.test_bs['id'])
         self.assertIn(self.response, Review.reviews)
 
     def test_returns_all_reviews_for_business(self):
         self.review.add_review(
-            4, "Nice place for holidays", 4, 1)
+            4, "Nice place for holidays",
+            self.test_user['username'], self.test_bs['id'])
         self.review.add_review(
-            1, "Chefs are rude", 4, 2)
-        self.assertTrue(len(self.review.view_reviews_for(1)) == 2)
+            1, "Chefs are rude",
+            self.test_user['username'], self.test_bs['id'])
+        self.assertTrue(len(self.review.view_reviews_for(
+            self.test_bs['id'])) == 2)
