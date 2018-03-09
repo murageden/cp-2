93c880d037e0785ff62289d72e2abb9254ba7289
dennis-murage <muragemd@gmail.com>
2018-03-09 13:31:02 +0300

    Add docstrings to tests

--- api/v1/tests/test_routes.py
+++ api/v1/tests/test_routes.py
@@ -66,6 +66,10 @@ class EndpointsTestCase(unittest.TestCase):
         }
 
     def test_create_user_without_some_required_data(self):
+        """
+            Tries to create a user, sending incomplete data
+            Expects an error, Resource should not be created
+        """
         self.response = self.client.post('/weconnect/api/v1/auth/register',
                     data=json.dumps(self.incomplete_user),
                     headers={'content-type': 'application/json'})
@@ -74,6 +78,10 @@ class EndpointsTestCase(unittest.TestCase):
         self.assertIn("Missing details", str(self.response.data))
 
     def test_create_user_with_correct_data(self):
+        """
+            Creates a user, sending all required data
+            Expects success, Resource should be created
+        """
         self.response = self.client.post('/weconnect/api/v1/auth/register',
                     data=json.dumps(self.test_user),
                     headers={'content-type': 'application/json'})
@@ -81,6 +89,10 @@ class EndpointsTestCase(unittest.TestCase):
         self.assertIn("Test Name", str(self.response.data))
 
     def test_login_user_with_correct_data(self):
+        """
+            Logs in a user, sending all required data
+            Expects success, success message expected
+        """
         self.response = self.client.post('/weconnect/api/v1/auth/login',
                     data=json.dumps(self.test_login),
                     headers={'content-type': 'application/json'})
@@ -91,6 +103,10 @@ class EndpointsTestCase(unittest.TestCase):
         self.assertEqual(user['email'], self.test_user['email'])
 
     def test_bad_login_returns_an_error(self):
+        """
+            Tries to log in a user, sending incomplete data
+            Expects an error
+        """
         self.response = self.client.post('/weconnect/api/v1/auth/login',
                     data=json.dumps(self.test_bad_login),
                     headers={'content-type': 'application/json'})
@@ -99,6 +115,11 @@ class EndpointsTestCase(unittest.TestCase):
         self.assertIn('Wrong', self.j_response['msg'])
 
     def test_register_business_with_correct_data_and_token(self):
+        """
+            Registers a business, sending all required data
+            Including the authentication token
+            Expects success, success message expected
+        """
         self.response = self.client.post('/weconnect/api/v1/auth/login',
                     data=json.dumps(self.test_login),
                     headers={'content-type': 'application/json'})
@@ -112,6 +133,11 @@ class EndpointsTestCase(unittest.TestCase):
         self.assertIn(self.j_response, Business.businesses)
 
     def test_register_business_without_token(self):
+        """
+            Tries to resister a business, sending all required data
+            Except the authentication token
+            Expects an error
+        """
         self.response = self.client.post('/weconnect/api/v1/businesses',
                     data=json.dumps(self.test_business),
                     headers={'content-type': 'application/json'})
@@ -121,6 +147,11 @@ class EndpointsTestCase(unittest.TestCase):
         self.assertNotIn(self.j_response, Business.businesses)
 
     def test_update_business_with_correct_data_and_token(self):
+        """
+            Updates a business, sending all required data
+            Expects success, success message expected
+            Resource should be updated
+        """
         self.response = self.client.post('/weconnect/api/v1/auth/login',
                     data=json.dumps(self.test_login),
                     headers={'content-type': 'application/json'})
@@ -140,6 +171,12 @@ class EndpointsTestCase(unittest.TestCase):
         self.assertIn(self.j_response, Business.businesses)
 
     def test_update_business_with_incorrect_data_and_token(self):
+        """
+            Tries to update a business, sending incomplete data
+            Including the auth token
+            Expects success, success message expected
+            Resource should be updated
+        """
         self.response = self.client.post('/weconnect/api/v1/auth/login',
                     data=json.dumps(self.test_login),
                     headers={'content-type': 'application/json'})
@@ -158,6 +195,12 @@ class EndpointsTestCase(unittest.TestCase):
         self.assertIn('Missing details', str(self.response.data))
 
     def test_update_business_without_token(self):
+        """
+            Tries to update a business, sending all required data
+            Except the auth token
+            Expects an error
+            Resource should not be updated
+        """
         self.response = self.client.put('/weconnect/api/v1/businesses/1',
                     data=json.dumps(self.new_business),
                     headers={'content-type': 'application/json'})
@@ -167,6 +210,11 @@ class EndpointsTestCase(unittest.TestCase):
         self.assertNotIn(self.j_response, Business.businesses)
 
     def test_delete_own_business_with_token(self):
+        """
+            Tries to delete a business, using the auth token
+            Expects success, success msg
+            Resource should be deleted
+        """
         self.response = self.client.post('/weconnect/api/v1/auth/login',
                     data=json.dumps(self.test_login),
                     headers={'content-type': 'application/json'})
@@ -189,6 +237,11 @@ class EndpointsTestCase(unittest.TestCase):
         self.assertNotIn(self.j_response, Business.businesses)
 
     def test_delete_business_without_token(self):
+        """
+            Tries to delete a business, without the auth token
+            Expects an error
+            Resource should not be deleted
+        """
         self.response = self.client.delete('/weconnect/api/v1/businesses/1',
                     data=json.dumps({}),
                     headers={'content-type': 'application/json'})
@@ -197,6 +250,11 @@ class EndpointsTestCase(unittest.TestCase):
         self.assertIn('Token is missing', self.j_response['msg'])
 
     def test_creates_a_review_for_a_business_with_token_and_correct_data(self):
+        """
+            Tries to create a review, using the auth token, with correct data
+            Expects success, success msg
+            Resource should be created
+        """
         self.response = self.client.post('/weconnect/api/v1/auth/login',
                     data=json.dumps(self.test_login),
                     headers={'content-type': 'application/json'})
@@ -216,6 +274,11 @@ class EndpointsTestCase(unittest.TestCase):
         self.assertIn(self.j_response, Review.reviews)
 
     def test_creates_a_review_for_a_business_with_token_and_incorrect_data(self):
+        """
+            Tries to create a review, using the auth token, passing incorrect data
+            Expects error
+            Resource should not be created
+        """
         self.response = self.client.post('/weconnect/api/v1/auth/login',
                     data=json.dumps(self.test_login),
                     headers={'content-type': 'application/json'})
@@ -235,9 +298,15 @@ class EndpointsTestCase(unittest.TestCase):
         self.assertNotIn(self.j_response, Review.reviews)
         self.assertIn("Ratings must be values", str(self.j_response))
 
-    def test_create_review_for_non_existing_business_returns_error(self):
+    def test_create_review_for_business_without_token_returns_error(self):
+        """
+            Tries to create a review for non-existing business
+            Without using the auth token
+            Expects error
+            Resource should not be created
+        """
         self.response = self.client.post(
-            '/weconnect/api/v1/businesses/1000/reviews',
+            '/weconnect/api/v1/businesses/1/reviews',
                     data=json.dumps(self.test_review),
                     headers={'content-type': 'application/json',
                     'x-access-token': 'nonthing here'})
