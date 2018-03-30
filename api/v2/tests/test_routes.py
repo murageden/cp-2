88b067b59a0175a1cf0d76df00956db037728c83
dennis-murage <muragemd@gmail.com>
2018-03-30 14:37:53 +0300

    Add tests for routes

---
+++ api/v2/tests/test_routes.py
@@ -0,0 +1,24 @@
+import unittest
+import os
+import json
+from we_connect import create_app, db
+
+class RoutesTestCase(unittest.TestCase):
+    """This class represents the routes test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client
+
+        # binds the app to the current context
+        with self.app.app_context():
+            # create all tables
+            db.create_all()
+    
+    def tearDown(self):
+        """teardown all initialized variables."""
+        with self.app.app_context():
+            # drop all tables
+            db.session.remove()
+            db.drop_all()
