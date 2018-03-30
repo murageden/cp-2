1b2ee1d96c9f6aa5ac2f58ac38590f27cb9bc28c
dennis-murage <muragemd@gmail.com>
2018-03-30 14:39:53 +0300

    Add data models

---
+++ api/v2/we_connect/models.py
@@ -0,0 +1,35 @@
+from we_connect import db
+
+class User(db.Model):
+    """This class represents the user table."""
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    username = db.Column(db.String, unique=True)
+    email = db.Column(db.String, unique=True)
+    passsword = db.Column(db.String)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(
+        db.DateTime, default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
+
+    def __init__(self, name, username, email, password):
+        """initialize with name."""
+        self.name = name
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all_users():
+        return User.query.all()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return "<User: {}>".format(self.username)
