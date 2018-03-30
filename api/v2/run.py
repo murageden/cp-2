77e805c6eb0201afb729c2481971be23be5e4afd
dennis-murage <muragemd@gmail.com>
2018-03-30 14:38:28 +0300

    Change application entry point

---
+++ api/v2/run.py
@@ -0,0 +1,9 @@
+import os
+
+from we_connect import create_app
+
+config_name = os.getenv('APP_CONFIGURATION') or 'development'
+app = create_app(config_name)
+
+if __name__ == '__main__':
+    app.run()
