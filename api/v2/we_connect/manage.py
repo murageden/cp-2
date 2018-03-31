3de77855c2a999922cbf3a0a6b57d15ccc74fd05
Toshiba <muragemd@gmail.com>
2018-03-31 14:10:59 +0300

    Add module to manage db migrations

--- api/v2/we_connect/manage.py
+++ api/v2/we_connect/manage.py
@@ -1,10 +1,9 @@
-"""Handle migrations"""
-import os
+"""Handle migrations."""
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from app import db, create_app
 
-app = create_app(config_name=os.getenv('APP_CONFIGURATION'))
+from run import db, app
+
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
