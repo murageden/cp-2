cfcb9d02d2a91a42e3fe0dfa542f846b226855cf
Toshiba <muragemd@gmail.com>
2018-04-01 02:04:40 +0300

    Add tests for business registration endpoint

---
+++ api/v2/tests/test_business_routes.py
@@ -0,0 +1,132 @@
+"""Contain tests for the user endpoints."""
+from flask import json
+import unittest
+# local imports
+from run import db
+from we_connect.routes import app
+
+
+class BusinessRoutesTestCase(unittest.TestCase):
+    """This class represents the business routes test case."""
+
+    def setUp(self):
+        """Define test variables."""
+        self.app = app
+        self.client = self.app.test_client()
+        # for registering a user
+        self.user = {
+            "name": "My Test Name",
+            "email": "test1@testing.com",
+            "username": "test1",
+            "password": "1234user"
+        }
+        # for logging in a user
+        self.login = {
+            "username": "test1",
+            "password": "1234user"
+        }
+        # for registering a bs
+        self.test_bs = {
+            "name": "Test Business",
+            "category": "shop",
+            "description": "The best prices in town",
+            "location": "Near TRM"
+        }
+        self.wrong_test_bs = {
+            "name": "  ",
+            "category": "shop",
+            "description": "The best prices in town",
+            "location": "Near TRM"
+        }
+
+        # binds the app to the current context
+        with self.app.app_context():
+            # create all tables
+            db.create_all()
+
+    def test_register_business_endpoint_without_token(self):
+        """Trying to register a business without a token."""
+        """Note no user has been created, no user logged in so far."""
+        self.response = self.client.post('/weconnect/api/v2/businesses',
+                                         data=json.dumps(self.test_bs),
+                                         headers={
+                                             'content-type': 'application/json'
+                                         })
+        self.assertEqual(self.response.status_code, 401)
+        self.assertIn("Token is missing", str(self.response.data))
+
+    def test_register_business_with_correct_token_and_data(self):
+        """Register a business with all input required."""
+        """Create a user, Login the user, grab the token, register bs."""
+        self.client.post('/weconnect/api/v2/auth/register',
+                         data=json.dumps(self.user),
+                         headers={
+                             'content-type': 'application/json'
+                         })
+        self.response = self.client.post('/weconnect/api/v2/auth/login',
+                                         data=json.dumps(self.login),
+                                         headers={
+                                             'content-type': 'application/json'
+                                         })
+        self.token = json.loads(self.response.data)['token']  # grab the token
+        self.response = self.client.post('/weconnect/api/v2/businesses',
+                                         data=json.dumps(self.test_bs),
+                                         headers={
+                                             'content-type': 'application/json',
+                                             'x-access-token': self.token
+                                         })
+        self.assertEqual(self.response.status_code, 201)
+        self.assertIn("The best prices in town",
+                      str(self.response.data))
+
+    def test_register_business_with_wrong_data(self):
+        """Try to register a business with empty name."""
+        self.client.post('/weconnect/api/v2/auth/register',
+                         data=json.dumps(self.user),
+                         headers={
+                             'content-type': 'application/json'
+                         })
+        self.response = self.client.post('/weconnect/api/v2/auth/login',
+                                         data=json.dumps(self.login),
+                                         headers={
+                                             'content-type': 'application/json'
+                                         })
+        self.token = json.loads(self.response.data)['token']  # grab the token
+        self.response = self.client.post('/weconnect/api/v2/businesses',
+                                         data=json.dumps(self.wrong_test_bs),
+                                         headers={
+                                             'content-type': 'application/json',
+                                             'x-access-token': self.token
+                                         })
+        self.assertEqual(self.response.status_code, 400)
+        self.assertIn("Empty details not allowed",
+                      str(self.response.data))
+
+    def test_register_business_with_a_wrong_token(self):
+        """Try to register a business while supplying an incorrect token."""
+        self.client.post('/weconnect/api/v2/auth/register',
+                         data=json.dumps(self.user),
+                         headers={
+                             'content-type': 'application/json'
+                         })
+        self.response = self.client.post('/weconnect/api/v2/auth/login',
+                                         data=json.dumps(self.login),
+                                         headers={
+                                             'content-type': 'application/json'
+                                         })
+        self.token = "a wrong token"
+        self.response = self.client.post('/weconnect/api/v2/businesses',
+                                         data=json.dumps(self.test_bs),
+                                         headers={
+                                             'content-type': 'application/json',
+                                             'x-access-token': self.token
+                                         })
+        self.assertEqual(self.response.status_code, 401)
+        self.assertIn("Token is invalid", str(self.response.data))
+
+    def tearDown(self):
+        """Tear down all initialized variables."""
+        with self.app.app_context():
+            # drop all tables
+            db.session.remove()
+            db.drop_all()
