0c019b452ba0af6b2e223d5296feec26a63dd8f7
murageden <muragemd@gmail.com>
2018-05-04 15:47:25 +0300

    Add import for page redirection, remove unused import

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request, redirect
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from datetime import timedelta
